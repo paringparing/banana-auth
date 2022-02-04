56d71fdb8963a6f1ef7b850f6a4cb70adb897d8c
파링 <parangee9706@gmail.com>
2022-02-04 15:26:36 +0900

    fix to use cwd

--- src/web/app.ts
+++ src/web/app.ts
@@ -3,11 +3,11 @@ import path from 'path'
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, '../../dist/client')))
+app.use(express.static(path.join(process.cwd(), 'dist/client')))
 
 app.use((req, res, next) => {
     if (!req.path.startsWith('/api')) {
-        res.sendFile(path.join(__dirname, '../../dist/client/app.html'))
+        res.sendFile(path.join(process.cwd(), 'dist/client/app.html'))
         return
     }
     return next()
